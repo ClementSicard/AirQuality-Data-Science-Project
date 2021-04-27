c2e8fdae03a1f28c8a71a8338a9e79184bfc9ece
Victor Gillioz <victor.gillioz@epfl.ch>
2021-04-27 17:26:35 +0200

    Get_data updated

--- get_data.py
+++ get_data.py
@@ -1,15 +1,16 @@
-import os
-from time import sleep
+import sys
+import time
+#import thingspeak
+import datetime
+import csv
 import Adafruit_DHT
-import requests
 from sps30 import SPS30
-from datetime import datetime
+from time import sleep
+from urllib.request import urlopen
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
-
-# In seconds
-SAMPLING_INTERVAL = 30
+#ThingsSpeak credentials
+#myAPI = 'XXXXXXXXXX' #Add your API Key
+#baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
 
 try:
     f = open('/home/pi/dts/info.csv', 'a+')
@@ -20,86 +21,100 @@ except:
     pass
 
 
-sps = SPS30(1)
+pm1, pm25, pm10 = None, None, None
+try:
+    sps = SPS30(1)
 
-if sps.read_article_code() == sps.ARTICLE_CODE_ERROR:
-    raise Exception("ARTICLE CODE CRC ERROR!")
-else:
-    print("ARTICLE CODE: " + str(sps.read_article_code()))
-
-if sps.read_device_serial() == sps.SERIAL_NUMBER_ERROR:
-    raise Exception("SERIAL NUMBER CRC ERROR!")
-else:
-    print("DEVICE SERIAL: " + str(sps.read_device_serial()))
-# default 604800, set 0 to disable auto-cleaning
-sps.set_auto_cleaning_interval(0)
-sps.device_reset()
-
-
-def temp_and_humidity():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-    if humidity is not None and temperature is not None:
-        return temperature, humidity
+    if sps.read_article_code() == sps.ARTICLE_CODE_ERROR:
+        raise Exception("ARTICLE CODE CRC ERROR!")
     else:
-        print("Failed to retrieve data from humidity sensor")
+        print("ARTICLE CODE: " + str(sps.read_article_code()))
+
+    if sps.read_device_serial() == sps.SERIAL_NUMBER_ERROR:
+        raise Exception("SERIAL NUMBER CRC ERROR!")
+    else:
+        print("DEVICE SERIAL: " + str(sps.read_device_serial()))
+
+    if sps.read_auto_cleaning_interval() != 604800 :
+        sps.set_auto_cleaning_interval(604800) # default 604800, set 0 to disable auto-cleaning
+        sps.device_reset() # device has to be powered-down or reset to check new auto-cleaning interval
+
+    if sps.read_auto_cleaning_interval() == sps.AUTO_CLN_INTERVAL_ERROR: # or returns the interval in seconds
+        raise Exception("AUTO-CLEANING INTERVAL CRC ERROR!")
+    else:
+        print("AUTO-CLEANING INTERVAL: " + str(sps.read_auto_cleaning_interval()))
+
+    sleep(5)
+
+    sps.start_measurement()
+
+    sleep(5)
+
+    while not sps.read_data_ready_flag():
+        sleep(0.25)
+        if sps.read_data_ready_flag() == sps.DATA_READY_FLAG_ERROR:
+            raise Exception("DATA-READY FLAG CRC ERROR!")
+
+    if sps.read_measured_values() == sps.MEASURED_VALUES_ERROR:
+        raise Exception("MEASURED VALUES CRC ERROR!")
+    else:
+        #print ("pm1
+        #.0 Value in µg/m3: " + str(sps.dict_values['pm1p0']))
+        #print ("PM2.5 Value in µg/m3: " + str(sps.dict_values['pm2p5']))
+        #print ("PM4.0 Value in µg/m3: " + str(sps.dict_values['pm4p0']))
+        #print ("pm10.0 Value in µg/m3: " + str(sps.dict_values['pm10p0']))
+        #print ("NC0.5 Value in 1/cm3: " + str(sps.dict_values['nc0p5']))    # NC: Number of Concentration
+        #print ("NC1.0 Value in 1/cm3: " + str(sps.dict_values['nc1p0']))
+        #print ("NC2.5 Value in 1/cm3: " + str(sps.dict_values['nc2p5']))
+        #print ("NC4.0 Value in 1/cm3: " + str(sps.dict_values['nc4p0']))
+        #print ("NC10.0 Value in 1/cm3: " + str(sps.dict_values['nc10p0']))
+        #print ("Typical Particle Size in µm: " + str(sps.dict_values['typical']))
+
+    sps.stop_measurement()
+
+    sps.start_fan_cleaning()
+
+    pm1 = round((sps.dict_values['pm1p0']),2)
+    pm25 = round((sps.dict_values['pm2p5']),2)
+    pm10 = round((sps.dict_values['pm10p0']),2)
+except:
+    print("SPS ERROR")
 
 
-def pollution():
-    try:
-        sleep(3)
-        sps.start_measurement()
-        sleep(3)
+temp, hum = None, None
+try:
+    sensor = Adafruit_DHT.DHT22
 
-        while not sps.read_data_ready_flag():
-            print("New Measurement is not available!")
-            sleep(1)
-            if sps.read_data_ready_flag() == sps.DATA_READY_FLAG_ERROR:
-                raise Exception("DATA-READY FLAG CRC ERROR!")
-        pm1 = 0
-        pm2 = 0
-        pm10 = 0
-        if sps.read_measured_values() == sps.MEASURED_VALUES_ERROR:
-            raise Exception("MEASURED VALUES CRC ERROR!")
-        else:
-            pm1 = str(sps.dict_values['pm1p0'])
-            pm2 = str(sps.dict_values['pm2p5'])
-            pm10 = str(sps.dict_values['pm10p0'])
+    # Set to your GPIO pin
+    pin    = 4
 
-        sps.stop_measurement()
-        print("Measurement added!")
-        sps.start_fan_cleaning()
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-        return pm1, pm2, pm10
-
-    except:
-        print("Oupsi erreur")
-        exit()
-        # A changer
+    temp = str(round((temperature),2))
+    hum = str(round((humidity),2))
+except:
+    print("ADAFRUIT ERROR")
 
 
-def save(date, time, temp, hum, pm1, pm25, pm10):
-    with open("/home/pi/dts/info.csv", "a+") as f:
-        f.write("{},{},{:0.1f},{:0.1f},{:0.5f},{:0.5f},{:0.5f}\n".format(
-            date, time, temp, hum, pm1, pm25, pm10))
+date = str(datetime.datetime.now().strftime("%Y-%m-%d"))
+time = str(datetime.datetime.now().strftime("%H:%M:%S"))
+
+# Specify the fields for ThingsSpeak and send data
+#f = urlopen(baseURL + '&field1=%s&field2=%s&field3=%s&field4=%s&field5=%s' % (temp, hum, pm1, pm25, pm10)) 
+#f.read()
+#f.close()
+
+with open("/home/pi/dts/info.csv", "a+") as f:
+    f.write("{},{},{},{},{},{},{}\n".format(date, time, temp, hum, pm1, pm25, pm10))
+
+# os.system("git commit -am '[Automatic] CSV udpdate' > /dev/null")
+# os.system("git push > /dev/null")
 
 
-def upload(t, h, pm1, pm25, pm10):
-    r = requests.get(
-        "https://api.thingspeak.com/update?api_key=L50MRPUQ7X1N3J9Z&field2={0}&field3={1}&field4={2:0.5f}&field5={3:0.5f}&field6={4:0.5f}".format(t, h, pm1, pm25, pm10))
-
-
-while True:
-    now = datetime.now()
-
-    t, h = temp_and_humidity()
-    pm1, pm25, pm10 = pollution()
-
-    dt_string = now.strftime("%d.%m.%Y %H:%M:%S")
-    date, time = dt_string.split(" ")
-
-    save(date, time, t, h, float(pm1), float(pm25), float(pm10))
-    upload(t, h, float(pm1), float(pm25), float(pm10))
-    # os.system("git commit -am '[Automatic] CSV udpdate' > /dev/null")
-    # os.system("git push > /dev/null")
-    sleep(SAMPLING_INTERVAL)
+print('Temperature in Celsius:',temp)
+print('Humidity in %:', hum)
+print('pm1 value in µg/m3:',pm1)
+print('PM2.5 value in µg/m3:',pm25)
+print('pm10 value in µg/m3:',pm10)
+print('Date:',date)
+print('Time:',time)
