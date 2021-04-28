6652a633d8c0d2ad599f4004083eeaabe9742ffe
Clément Sicard <clement.sicard@epfl.ch>
2021-04-28 15:36:45 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -9,8 +9,8 @@ from sps30 import SPS30
 from time import sleep
 from urllib.request import urlopen
 
-#ThingsSpeak credentials
-myAPI = 'L50MRPUQ7X1N3J9Z' #Add your API Key
+# ThingsSpeak credentials
+myAPI = 'L50MRPUQ7X1N3J9Z'  # Add your API Key
 baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
 
 
@@ -20,16 +20,17 @@ try:
 
     if sps.read_article_code() == sps.ARTICLE_CODE_ERROR:
         raise Exception("ARTICLE CODE CRC ERROR!")
-        
 
     if sps.read_device_serial() == sps.SERIAL_NUMBER_ERROR:
         raise Exception("SERIAL NUMBER CRC ERROR!")
 
-    if sps.read_auto_cleaning_interval() != 604800 :
-        sps.set_auto_cleaning_interval(604800) # default 604800, set 0 to disable auto-cleaning
-        sps.device_reset() # device has to be powered-down or reset to check new auto-cleaning interval
+    if sps.read_auto_cleaning_interval() != 604800:
+        # default 604800, set 0 to disable auto-cleaning
+        sps.set_auto_cleaning_interval(604800)
+        # device has to be powered-down or reset to check new auto-cleaning interval
+        sps.device_reset()
 
-    if sps.read_auto_cleaning_interval() == sps.AUTO_CLN_INTERVAL_ERROR: # or returns the interval in seconds
+    if sps.read_auto_cleaning_interval() == sps.AUTO_CLN_INTERVAL_ERROR:  # or returns the interval in seconds
         raise Exception("AUTO-CLEANING INTERVAL CRC ERROR!")
 
     sleep(5)
@@ -45,12 +46,12 @@ try:
 
     if sps.read_measured_values() == sps.MEASURED_VALUES_ERROR:
         raise Exception("MEASURED VALUES CRC ERROR!")
-    #else:
+    # else:
         #print ("pm1.0 Value in µg/m3: " + str(sps.dict_values['pm1p0']))
         #print ("PM2.5 Value in µg/m3: " + str(sps.dict_values['pm2p5']))
         #print ("PM4.0 Value in µg/m3: " + str(sps.dict_values['pm4p0']))
         #print ("pm10.0 Value in µg/m3: " + str(sps.dict_values['pm10p0']))
-        #print ("NC0.5 Value in 1/cm3: " + str(sps.dict_values['nc0p5']))    # NC: Number of Concentration
+        # print ("NC0.5 Value in 1/cm3: " + str(sps.dict_values['nc0p5']))    # NC: Number of Concentration
         #print ("NC1.0 Value in 1/cm3: " + str(sps.dict_values['nc1p0']))
         #print ("NC2.5 Value in 1/cm3: " + str(sps.dict_values['nc2p5']))
         #print ("NC4.0 Value in 1/cm3: " + str(sps.dict_values['nc4p0']))
@@ -61,9 +62,9 @@ try:
 
     sps.start_fan_cleaning()
 
-    pm1 = round((sps.dict_values['pm1p0']),2)
-    pm25 = round((sps.dict_values['pm2p5']),2)
-    pm10 = round((sps.dict_values['pm10p0']),2)
+    pm1 = round((sps.dict_values['pm1p0']), 2)
+    pm25 = round((sps.dict_values['pm2p5']), 2)
+    pm10 = round((sps.dict_values['pm10p0']), 2)
 except Exception as e:
     print(e)
 
@@ -73,12 +74,12 @@ try:
     sensor = Adafruit_DHT.DHT22
 
     # Set to your GPIO pin
-    pin    = 4
+    pin = 4
 
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-    temp = str(round((temperature),2))
-    hum = str(round((humidity),2))
+    temp = str(round((temperature), 2))
+    hum = str(round((humidity), 2))
 except:
     print("ADAFRUIT ERROR")
 
@@ -88,17 +89,19 @@ time = str(datetime.datetime.now().strftime("%H:%M:%S"))
 
 # Specify the fields for ThingsSpeak and send data
 try:
-    f = urlopen(baseURL + '&field2=%s&field3=%s&field4=%s&field5=%s&field6=%s' % (temp, hum, pm1, pm25, pm10)) 
+    f = urlopen(baseURL + '&field2=%s&field3=%s&field4=%s&field5=%s&field6=%s' %
+                (temp, hum, pm1, pm25, pm10))
     f.read()
     f.close()
 except:
     print("Erreur reseau")
 
 with open("/home/pi/dts/info.csv", "a+") as f:
-    f.write("{},{},{},{},{},{},{}\n".format(date, time, temp, hum, pm1, pm25, pm10))
+    f.write("{},{},{},{},{},{},{}\n".format(
+        date, time, temp, hum, pm1, pm25, pm10))
 
 os.system("git commit -am '[Automatic] CSV udpdate' &> /dev/null")
-os.system("git push > /dev/null")
+os.system("git push &> /dev/null")
 
 
 #print('Temperature in Celsius:',temp)
@@ -106,6 +109,6 @@ os.system("git push > /dev/null")
 #print('pm1 value in µg/m3:',pm1)
 #print('PM2.5 value in µg/m3:',pm25)
 #print('pm10 value in µg/m3:',pm10)
-#print('Date:',date)
-#print('Time:',time)
+# print('Date:',date)
+# print('Time:',time)
 print("New measurement uploaded!\n")
